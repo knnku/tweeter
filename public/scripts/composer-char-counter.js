5539efe90fb818c71b49b2329bc78042d46c68e8
Kenniku Tolato <kenniku_you@ymail.com>
2023-09-07 06:10:24 +0000

    Implement character counter.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // --- our code goes here ---
 
   console.log("Dom is loaded: ready!");
 
-  $("#tweet-text").on('keypress', function() {
-    console.log(this);
-  })
+  $("#tweet-text").on("input", function () {
+    let tweetCount = $(this).val().length;
+    let target = $(this).parent().find(".counter");
+  
+    let remainingCharacters = 140 - tweetCount;
+    target.val(remainingCharacters);
 
+    //Red
+    if (remainingCharacters <= 0) {
+      target.css("color", "red");
+    } else {
+      target.css("color", ""); 
+    }
+
+  });
 });
-
-
-
-
